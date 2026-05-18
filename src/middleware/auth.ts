6a7f71c1cc89f5950a5d0544ab5640b696feb7f7
Kevin <kevinklgvg@gmail.com>
2026-05-18 13:21:25 -0300

    middleware auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,46 +6,39 @@ export interface AuthRequest extends Request {
   user?: User;
 }
 
-export const authMiddleware = async (
-  req: AuthRequest,
-  res: Response,
-  next: NextFunction,
-) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(" ")[1];
+export class AuthMiddleware {
+  async authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.split(" ")[1];
+    if (!token) return res.status(401).json({ error: "Token not provided" });
 
-  if (!token) return res.status(401).json({ error: "Token not provided" });
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser(token);
 
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser(token);
+    if (error || !user)
+      return res.status(401).json({ error: "Invalid or expired session" });
 
-  if (error || !user)
-    return res.status(401).json({ error: "Invalid or expired session" });
+    if (user.user_metadata?.is_active === false)
+      return res.status(403).json({ error: "User inactive" });
 
-  req.user = user;
-  next();
-};
-
-export const isAdmin = (
-  req: AuthRequest,
-  res: Response,
-  next: NextFunction,
-) => {
-  const user = req.user;
-
-  if (!user) {
-    return res.status(401).json({ error: "User not authenticated" });
+    req.user = user;
+    next();
   }
 
-  const profile = user.user_metadata?.profile;
+  async isManager(req: AuthRequest, res: Response, next: NextFunction) {
+    const user = req.user;
+    if (!user) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+    const profile = user.user_metadata?.profile;
 
-  if (profile !== "admin") {
-    return res.status(403).json({
-      error: "Access denied: Administrators only",
-    });
+    if (profile !== "manager") {
+      return res.status(403).json({
+        error: "Access denied: Administrators only",
+      });
+    }
+    next();
   }
-
-  next();
-};
+}
