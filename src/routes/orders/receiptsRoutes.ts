ce9cf3e8dddd224a9247b3afdf0d87ce6e64c0c5
kevin Oliveira <kevinklgvg@gmail.com>
2026-05-29 16:45:10 -0300

    delete itens

--- src/routes/orders/receiptsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { authMiddleware } from '@/middleware/auth';
-import { orderReceiptsController } from '@/controllers/orders/OrderReceiptsController';
-
-const router = express.Router();
-router.get   ("/items/:item_id/receipts", orderReceiptsController.findByItem);
-router.post  ("/receipts", orderReceiptsController.create);
-
-export default router;
